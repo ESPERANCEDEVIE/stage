5af474225a590e997f8d8a9835ad99dda4434988
ESPERANCEDEVIE <157617149+ESPERANCEDEVIE@users.noreply.github.com>
2024-02-09 10:29:11 +0100

    Add files via upload

---
+++ php.max/controllers/c_connexion.php
@@ -0,0 +1,39 @@
+﻿<?php
+if (!isset($_REQUEST['action'])) {
+    $_REQUEST['action'] = 'demandeConnexion';
+}
+$action = $_REQUEST['action'];
+switch ($action) {
+    case 'demandeConnexion':
+    {
+        include("/views/v_connexion.php");
+        break;
+    }
+    case 'valideConnexion':
+    {
+        $login = $_REQUEST['login'];
+        $mdp = $_REQUEST['mdp'];
+        /** @var Pdoasso $pdo */
+        $visiteur = $Pdo->getVisiteur($login, $mdp);
+        if (!is_array($visiteur)) {
+            ajouterErreur("Login ou mot de passe incorrect");
+            include("views/v_erreurs.php");
+            include("views/v_connexion.php");
+        } else {
+            include 'sfonction.php';
+        }
+        break;
+    }
+    case 'deconnexion':
+    {
+        deconnecter();
+        include'views/v_menu.php';
+        include("views/v_connexion.php");
+        break;
+    }
+    default :
+    {
+        include("views/v_connexion.php");
+        break;
+    }
+}
