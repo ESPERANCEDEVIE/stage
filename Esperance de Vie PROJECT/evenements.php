4154a65519283bf3a3237d3d370f104fb7e55e70
ESPERANCEDEVIE <157617149+ESPERANCEDEVIE@users.noreply.github.com>
2024-02-12 22:55:00 +0100

    Add files via upload

---
+++ Esperance de Vie PROJECT/evenements.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esperance de vie</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/3010b1eaf1.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    
+    <!-- barre de nav  -->
+    <header>
+      
+        
+        <div class="menu_toggle">
+            <span></span>
+        </div>
+
+        <div class="logo">
+            <p><span>Esperance</span><span> de</span> Vie</p>
+        </div>
+        <ul class="menu">
+            <li><a href="index.php">Accueil</a></li>
+            <li class="dropdown">
+                <a href="#" class="dropbtn">Faire un don</a>
+                <div class="dropdown-content">
+                    <a href="materiel.php">Matériel</a>
+                    <a href="financier.php">Financier</a>
+                </div>
+            </li>
+            <li><a href="benevole.php">Adhérant</a></li>
+            <li><a href="evenements.php">Evenements</a></li>
+            <li><a href="contact.php">Nous contacter</a></li>
+            
+        </ul>
+        <a href="connexion.php" class="login_btn"><button>Connexion</button></a>
+       
+    </header>
+<!-- evenements  -->
+<section id="évenements">
+        <h1 class="section_title">Nos évenements</h1>
+        <div class="images">
+            <ul>
+                <li class="evenements">
+                   <div>
+                       <img src="" alt="">
+                   </div>
+                   <span>Voici notre invitation pour notre prochain Dîner Dansant de l'association. Les réservations sont à faire avant le 20 février prochain....faite vite 🥰</span>
+                   <span class="#"></span>
+                   <a href=""></a>
+                </li>
+                <li class="evenements">
+                    <div>
+                        <img src="" alt="">
+                    </div>
+                    <span></span>
+                    <span class="#"></span>
+                    <a href="#"></a>
+                 </li>
+                 <li class="evenements">
+                    <div>
+                        <img src="" alt="">
+                    </div>
+                    <span></span>
+                    <span class="#"></span>
+                    <a href="#"></a>
+                 </li>
+                 <li class="evenements">
+                    <div>
+                        <img src="" alt="">
+                    </div>
+                    <span></span>
+                    <span class="#"></span>
+                    <a href="#"></a>
+                 </li>
+                 <li class="evenements">
+                    <div>
+                        <img src="" alt="">
+                    </div>
+                    <span></span>
+                    <span class="#"></span>
+                    <a href="#"></a>
+                 </li>
+                 <li class="evenements">
+                    <div>
+                        <img src="" alt="">
+                    </div>
+                    <span></span>
+                    <span class="#"></span>
+                    <a href="#"></a>
+                 </li>
+            </ul>
+        </div>
+    </section>
